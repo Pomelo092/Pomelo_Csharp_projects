31a0826aa81a546052ffe9c84fbaef7ab2ed3dfa
Pomelo092 <pomyalov2617@gmail.com>
2025-09-20 18:32:58 +0300

    треугольник

---
+++ Triangle_opred/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите первую сторону треугольника: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите вторую сторону треугольника: ");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите третью сторону треугольника: ");
+    int c = int.Parse(Console.ReadLine());
+
+    if (a == b && b == c)
+    {
+        Console.WriteLine("Равносторонний");
+    }
+    else if (a == b && b != c || b == c && c != a || c == a && a != b)
+    {
+        Console.WriteLine("Равнобедренный");
+    }
+    else 
+    {
+        Console.WriteLine("Разносторонний");
+    }
+}
